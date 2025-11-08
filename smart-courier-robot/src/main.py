57f231222425bf0992f834ff324584579d6d3afe
Malak-Oualid <144572770+Malak-Oualid@users.noreply.github.com>
2025-11-08 16:24:13 -0500

    test rotate

--- smart-courier-robot/src/main.py
+++ smart-courier-robot/src/main.py
@@ -8,6 +8,18 @@ T_SENSOR = TouchSensor(2) # Touch Sensor in Port S2
 LEFT_MOTOR = Motor("A")   # Left motor in Port A
 RIGHT_MOTOR = Motor("D")  # Right motor in Port D
 
+def rotate(angle, speed):
+    try:
+        set_motor_limits(LEFT_MOTOR, POWER_LIMIT, speed)   # Set speed
+        set_motor_limits(RIGHT_MOTOR, POWER_LIMIT, speed)
+        set_motor_position_relative(LEFT_MOTOR, int(angle * ORIENTTODEG))   # Rotate L wheel +ve
+        set_motor_position_relative(RIGHT_MOTOR, int(-angle * ORIENTTODEG)) # Rotate R wheel -ve
+        Block(RIGHT_MOTOR)
+        print("i wanna rotate")
+    except IOError as error:
+        print(error)
+
+
 try:
     wait_ready_sensors() # Wait for sensors to initialize
     
@@ -19,7 +31,8 @@ try:
         try:
             if T_SENSOR.is_pressed(): # Press touch sensor to stop robot
                 print("Button pressed")
-                BP.reset_all()
+                #BP.reset_all()
+                rotate(90, 20)
                 exit()
             time.sleep(SENSOR_POLL_SLEEP) # Use sensor polling interval here
         except SensorError as error:
