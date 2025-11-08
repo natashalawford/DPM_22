17f14c87ce4b1c71b070d3ef949e8c21214bf268
natashalawford <natasha.lawford@gmail.com>
2025-11-08 16:17:31 -0500

    drive straight with emergency stop

--- smart-courier-robot/src/main.py
+++ smart-courier-robot/src/main.py
@@ -1,21 +1,24 @@
 from utils.brick import BP, TouchSensor, Motor, wait_ready_sensors, SensorError
 import time
 
-FORWARD_SPEED = 20        # speed constant = 30% power
+FORWARD_SPEED = 75        # speed constant = 30% power
 SENSOR_POLL_SLEEP = 0.05  # Polling rate = 50 msec
 
-T_SENSOR = TouchSensor(1) # Touch Sensor in Port S1
+T_SENSOR = TouchSensor(2) # Touch Sensor in Port S2
 LEFT_MOTOR = Motor("A")   # Left motor in Port A
 RIGHT_MOTOR = Motor("D")  # Right motor in Port D
 
 try:
     wait_ready_sensors() # Wait for sensors to initialize
+    
     LEFT_MOTOR.set_power(FORWARD_SPEED) # Start left motor
     RIGHT_MOTOR.set_power(FORWARD_SPEED) # Simultaneously start right motor
+    print("motors set up and running")
 
     while True:
         try:
             if T_SENSOR.is_pressed(): # Press touch sensor to stop robot
+                print("Button pressed")
                 BP.reset_all()
                 exit()
             time.sleep(SENSOR_POLL_SLEEP) # Use sensor polling interval here
