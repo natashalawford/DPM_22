e879d8a7bd086e0462dcbb9fe195306a7387561c
Malak Oualid <malak.oualid@mail.mcgill.ca>
2025-11-14 14:49:23 -0500

    natasha updated dropoff

--- smart-courier-robot/src/drop_off.py
+++ smart-courier-robot/src/drop_off.py
@@ -2,64 +2,91 @@ from utils.brick import BP, TouchSensor, Motor, wait_ready_sensors, SensorError
 import time
 import math
 
-#DRIVING
-BACKWARD_SPEED = 25       # speed constant = 25% power
-FORWARD_SPEED = 30        # speed constant = 30% power
-SENSOR_POLL_SLEEP = 0.05  # Polling rate = 50 msec
-BACKWARD_ROTATION = -1440  # rotation amount to back up in DEG
-IO_OFFICE_ROTATION = 1080  # rotation amount to enter office in DEG
-
-#TURNING
-WHEEL_RADIUS = 0.022
-AXEL_LENGTH = 0.078
-ORIENTTODEG = AXEL_LENGTH / WHEEL_RADIUS
-POWER_LIMIT = 80
 MOTOR_POLL_DELAY = 0.05
+
+SQUARE_LENGTH = 0.5     # (meters) Side length of square in
+WHEEL_RADIUS = 0.022    # (meters) Radius of one wheel
+AXLE_LENGTH = 0.077      # (meters) Distance between wheel contact with ground
+
+DIST_TO_DEG = 180/(math.pi*WHEEL_RADIUS)
+ORIENT_TO_DEG = AXLE_LENGTH / WHEEL_RADIUS # Convert whole robot rotation to wheel rotation
+
+FMD_SPEED = 150         # (deg per sec) Moving forward speed
+TRN_SPEED = 180         # (deg per sec) Turning a corner speed
+
+LEFT_MOTOR = Motor("A") # Left motor in Port A
+RIGHT_MOTOR = Motor("D") # Right motor in Port D
+POWER_LIMIT = 80        # Power limit = 80%
+SPEED_LIMIT = 720       # Speed limit = 720dps
 
 #PORTS
 T_SENSOR = TouchSensor(1) # colour Sensor in Port S1 CHANGE WHEN IN LAB
 LEFT_MOTOR = Motor("A")   # Left motor in Port A
 RIGHT_MOTOR = Motor("D")  # Right motor in Port D
+SERVO_MOTOR = Motor("B")  # Right motor in Port D
+SWEEP_ARM = Motor("C")
 
-# USED FOR TURNING
 def wait_for_motor(motor: Motor):
     while math.isclose(motor.get_speed(), 0):
         time.sleep(MOTOR_POLL_DELAY)
     while not math.isclose(motor.get_speed(), 0):
         time.sleep(MOTOR_POLL_DELAY)
 
-# USED FOR TURNING        
+
+def init_motor(motor: Motor):
+    """Function to initialize a motor"""
+    try:
+        motor.reset_encoder() # Reset encoder to 0 value
+        motor.set_limits(POWER_LIMIT, SPEED_LIMIT) # Set power and speed limits
+        motor.set_power(0) # Stop the motor as well
+    except IOError as error:
+        print(error)
+
+
+def move_dist_fwd(distance, speed): # meters, dps
+    """Function to move forward by user-specified distance and speed"""
+    try:
+        LEFT_MOTOR.set_dps(speed)
+        RIGHT_MOTOR.set_dps(speed) # Set speeds of motors
+        LEFT_MOTOR.set_limits(POWER_LIMIT, speed)
+        RIGHT_MOTOR.set_limits(POWER_LIMIT, speed)
+        LEFT_MOTOR.set_position_relative(int(distance * DIST_TO_DEG)) # Rotate wheels
+        RIGHT_MOTOR.set_position_relative(int(distance * DIST_TO_DEG))
+
+        wait_for_motor(RIGHT_MOTOR)
+    except IOError as error:
+        print(error)
+
+
 def rotate(angle, speed):
     try:
         LEFT_MOTOR.set_dps(speed)
         RIGHT_MOTOR.set_dps(speed)
         LEFT_MOTOR.set_limits(POWER_LIMIT, speed)   # Set speed
         RIGHT_MOTOR.set_limits(POWER_LIMIT, speed)   # Set speed
-        LEFT_MOTOR.set_position_relative(int(angle * ORIENTTODEG))   # Rotate L wheel +ve
-        RIGHT_MOTOR.set_position_relative(int(-angle * ORIENTTODEG)) # Rotate R wheel -ve
+        LEFT_MOTOR.set_position_relative(int(angle * ORIENT_TO_DEG))   # Rotate L wheel +ve
+        RIGHT_MOTOR.set_position_relative(int(-angle * ORIENT_TO_DEG)) # Rotate R wheel -ve
         wait_for_motor(RIGHT_MOTOR)
     except IOError as error:
         print(error)
-        
+
+def drop_package():
+    try:
+        #SERVO_MOTOR.set_limits(POWER_LIMIT, 20)
+        SERVO_MOTOR.set_position_relative(90)
+        wait_for_motor(SERVO_MOTOR)
+        print("dropped off")
+    except IOError as error:
+        print(error)
 
 
 try:
     wait_ready_sensors() # Wait for sensors to initialize
-
-    LEFT_MOTOR.set_limits(POWER_LIMIT, BACKWARD_SPEED)
-    RIGHT_MOTOR.set_limits(POWER_LIMIT, BACKWARD_SPEED)
-    LEFT_MOTOR.set_position_relative(BACKWARD_ROTATION) # Set distance to back up
-    RIGHT_MOTOR.set_position_relative(BACKWARD_ROTATION) # Set distance to back up simultaneously
-    print("robot backing up")
-
-    # Now Turn 90 degrees facing the office
-    rotate(90, 180)
-    print("robot turned")
+    print('Dropping off')
+    #init_motor(LEFT_MOTOR) # Initialize L Motor
+    #init_motor(RIGHT_MOTOR) # Initialize R Motor
     
-    # Move forward into the office
-    LEFT_MOTOR.set_position_relative(IO_OFFICE_ROTATION) # Set distance to move forward
-    RIGHT_MOTOR.set_position_relative(IO_OFFICE_ROTATION) # Set distance to move forward simultaneously
-    print("robot moving into office")
+    drop_package()
 
-except KeyboardInterrupt: # Allows program to be stopped on keyboard interrupt
+except KeyboardInterrupt: # Abort program using ^C (Ctrl+C)
     BP.reset_all()
